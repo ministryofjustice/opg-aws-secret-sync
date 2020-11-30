6f0f11d4388c19bebf39a9b9e99b9bbe346b70fd
Tom Withers <thomas.withers@digital.justice.gov.uk>
2020-11-30 11:49:48 +0000

    SP-549 - Create Lambda Function (#1)
    * SP-549 - Add Terraform Module
    * SP-549 - Add build pipeline

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/ministryofjustice/opg-aws-secret-sync
+
+go 1.15
+
+require (
+	github.com/aws/aws-lambda-go v1.20.0
+	github.com/aws/aws-sdk-go v1.35.31
+)
---
+++ terraform/iam.tf
@@ -0,0 +1,61 @@
+resource "aws_iam_role" "lambda_role" {
+  assume_role_policy = data.aws_iam_policy_document.lambda_assume.json
+  name_prefix        = "replicate-secrets-lambda-role"
+  tags               = var.tags
+}
+
+data "aws_iam_policy_document" "lambda_assume" {
+  statement {
+    actions = ["sts:AssumeRole"]
+
+    principals {
+      type        = "Service"
+      identifiers = ["lambda.amazonaws.com"]
+    }
+  }
+}
+
+resource "aws_iam_role_policy" "lambda" {
+  name   = "replicate-secrets-policy"
+  role   = aws_iam_role.lambda_role.id
+  policy = data.aws_iam_policy_document.lambda.json
+}
+
+data "aws_iam_policy_document" "lambda" {
+  statement {
+    sid       = "allowLogging"
+    effect    = "Allow"
+    resources = [aws_cloudwatch_log_group.lambda.arn]
+    actions = [
+      "logs:CreateLogStream",
+      "logs:PutLogEvents",
+      "logs:DescribeLogStreams"
+    ]
+  }
+
+  statement {
+    sid       = "SecretsManagerOriginRegion"
+    effect    = "Allow"
+    resources = ["arn:aws:kms:${var.source_secret_region}:${var.account_id}:*"]
+    actions = [
+      "secretsmanager:DescribeSecret",
+      "secretsmanager:GetSecretValue"
+    ]
+  }
+
+  statement {
+    sid       = "SecretsManagerReplicaRegion"
+    effect    = "Allow"
+    resources = ["arn:aws:kms:${var.target_secret_region}:${var.account_id}:*"]
+    actions = [
+      "secretsmanager:UpdateSecretVersionStage",
+      "secretsmanager:PutSecretValue",
+      "secretsmanager:DescribeSecret"
+    ]
+  }
+}
+
+resource "aws_iam_role_policy_attachment" "vpc_access_execution_role" {
+  role       = aws_iam_role.lambda_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaVPCAccessExecutionRole"
+}
